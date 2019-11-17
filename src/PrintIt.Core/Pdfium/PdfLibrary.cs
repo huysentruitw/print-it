e975aa62ff05a51458949d7cfb71281408fd7626
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2019-11-17 07:59:41 +0100

    Add PdfLibrary

---
+++ src/PrintIt.Core/Pdfium/PdfLibrary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PrintIt.Core.Pdfium
+{
+    public sealed class PdfLibrary : IDisposable
+    {
+        private static readonly object SyncRoot = new object();
+        private static PdfLibrary _library;
+
+        public static void EnsureInitialized()
+        {
+            lock (SyncRoot)
+            {
+                _library = _library ?? new PdfLibrary();
+            }
+        }
+
+        private bool _disposed;
+
+        private PdfLibrary()
+        {
+            NativeMethods.Init();
+        }
+
+        ~PdfLibrary()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+            NativeMethods.Destroy();
+            _disposed = true;
+        }
+    }
+}
