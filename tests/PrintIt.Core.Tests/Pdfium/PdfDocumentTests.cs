d9392a8b11d24d020bb2f37f19ddcde64817e290
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2019-11-19 19:03:11 +0100

    Add tests for PdfDocument

---
+++ tests/PrintIt.Core.Tests/Pdfium/PdfDocumentTests.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using FluentAssertions;
+using PrintIt.Core.Pdfium;
+using Xunit;
+
+namespace PrintIt.Core.Tests.Pdfium
+{
+    public sealed class PdfDocumentTests
+    {
+        [Fact]
+        public void Open_CanOpenDocumentFromStream()
+        {
+            // Arrange
+            PdfLibrary.EnsureInitialized();
+            using var stream = GetEmbeddedResourceStream("dummy.pdf");
+            
+            // Act
+            using var document = PdfDocument.Open(stream);
+            
+            // Assert
+            document.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void PageCount_ReturnsPageCountOfDocument()
+        {
+            // Arrange
+            PdfLibrary.EnsureInitialized();
+            using var stream = GetEmbeddedResourceStream("dummy.pdf");
+            using var document = PdfDocument.Open(stream);
+            
+            // Act
+            var pageCount = document.PageCount;
+            
+            // Assert
+            pageCount.Should().Be(1);
+        }
+        
+        private static Stream GetEmbeddedResourceStream(string name)
+        {
+            var type = typeof(PdfDocumentTests);
+            return type.Assembly.GetManifestResourceStream(type, name);
+        }
+    }
+}
