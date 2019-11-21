9002d6c22c05f656328a4a8157aca04250ecef91
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2019-11-21 18:32:03 +0100

    Add test for document property

---
+++ tests/PrintIt.Core.Tests/Pdfium/PdfPageTests.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using FluentAssertions;
+using PrintIt.Core.Pdfium;
+using Xunit;
+
+namespace PrintIt.Core.Tests.Pdfium
+{
+    public sealed class PdfPageTests
+    {
+        [Fact]
+        public void Document_ShouldPointToDocumentContainingPage()
+        {
+            // Arrange
+            PdfLibrary.EnsureInitialized();
+            using var stream = GetEmbeddedResourceStream("dummy.pdf");
+            using var document = PdfDocument.Open(stream);
+            using var page = document.OpenPage(0);
+            
+            // Act & Assert
+            page.Document.Should().Be(document);
+        }
+        
+        private static Stream GetEmbeddedResourceStream(string name)
+        {
+            var type = typeof(PdfDocumentTests);
+            return type.Assembly.GetManifestResourceStream(type, name);
+        }
+    }
+}
