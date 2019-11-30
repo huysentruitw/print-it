d1d7a3dafe94def7a6cc73e5aea3d4992f88ae66
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2019-11-30 23:25:53 +0100

    Add scaling and auto-rotation to page rendering

--- src/PrintIt.Core/Pdfium/PdfPage.cs
+++ src/PrintIt.Core/Pdfium/PdfPage.cs
@@ -31,16 +31,56 @@ namespace PrintIt.Core.Pdfium
             }
         }
 
-        public void RenderTo(Graphics graphics)
+        public void RenderTo(Graphics graphics, bool maintainAspectRatio = true,  bool allowUpscaling = false, bool center = true)
         {
+            bool rotate = false;
+            var pageSize = SizeInInch;
+
+            var pageAspect = pageSize.Width / pageSize.Height;
+            var destinationAspect = graphics.VisibleClipBounds.Width / graphics.VisibleClipBounds.Height;
+
+            if ((pageAspect >= 1.0f && destinationAspect < 1.0f) ||
+                (pageAspect < 1.0f && destinationAspect >= 1.0f))
+            {
+                rotate = true;
+            }
+
+            var pageWidthInPixels = (rotate ? pageSize.Height : pageSize.Width) * graphics.DpiX;
+            var pageHeightInPixels = (rotate ? pageSize.Width : pageSize.Height) * graphics.DpiY;
+            
+            var scaleX = graphics.VisibleClipBounds.Width / pageWidthInPixels;
+            var scaleY = graphics.VisibleClipBounds.Height / pageHeightInPixels;
+
+            if (!allowUpscaling)
+            {
+                scaleX = Math.Min(scaleX, 1.0f);
+                scaleY = Math.Min(scaleY, 1.0f);
+            }
+
+            if (maintainAspectRatio)
+            {
+                scaleX = scaleY = Math.Min(scaleX, scaleY);
+            }
+
+            var destinationWidth = Math.Min(pageWidthInPixels * scaleX, graphics.VisibleClipBounds.Width);
+            var destinationHeight = Math.Min(pageHeightInPixels * scaleY, graphics.VisibleClipBounds.Height);
+
+            var offsetX = 0.0f;
+            var offsetY = 0.0f;
+            if (center)
+            {
+                offsetX = (graphics.VisibleClipBounds.Width - destinationWidth) / 2.0f;
+                offsetY = (graphics.VisibleClipBounds.Height - destinationHeight) / 2.0f;
+            }
+            
             var boundingBox = new Rectangle(
-                x: (int)graphics.VisibleClipBounds.X,
-                y: (int)graphics.VisibleClipBounds.Y,
-                width: (int)graphics.VisibleClipBounds.Width,
-                height: (int)graphics.VisibleClipBounds.Height);
+                x: (int)(graphics.VisibleClipBounds.X + offsetX),
+                y: (int)(graphics.VisibleClipBounds.Y + offsetY),
+                width: (int)destinationWidth,
+                height: (int)destinationHeight);
 
             NativeMethods.RenderPage(_pageHandle, graphics.GetHdc(), boundingBox,
-                pageOrientation: NativeMethods.PageOrientation.Normal,
+                pageOrientation: rotate ? NativeMethods.PageOrientation.Rotated90DegreesCounterClockwise : NativeMethods.PageOrientation.Normal,
                 flags: NativeMethods.PageRenderingFlags.OptimizeForPrinting);
         }
     }
--- tests/PrintIt.Core.Tests/Pdfium/PdfPageTests.cs
+++ tests/PrintIt.Core.Tests/Pdfium/PdfPageTests.cs
@@ -35,8 +35,8 @@ namespace PrintIt.Core.Tests.Pdfium
             var size = page.SizeInInch;
             
             // Assert
-            size.Width.Should().BeApproximately(8.26f, 0.01f);
-            size.Height.Should().BeApproximately(11.69f, 0.01f);
+            size.Width.Should().BeApproximately(8.5f, 0.01f);
+            size.Height.Should().BeApproximately(11.0f, 0.01f);
         }
 
         [Fact]
@@ -47,8 +47,9 @@ namespace PrintIt.Core.Tests.Pdfium
             using var stream = GetEmbeddedResourceStream("dummy.pdf");
             using var document = PdfDocument.Open(stream);
             using var page = document.OpenPage(0);
-            using var bitmap = new Bitmap(2000, 1000, PixelFormat.Format24bppRgb);
-
+            using var bitmap = new Bitmap(1000, 1000, PixelFormat.Format24bppRgb);
+            bitmap.SetResolution(300.0f, 300.0f);
+            
             // Act
             using (var graphics = Graphics.FromImage(bitmap))
             {
@@ -57,7 +58,23 @@ namespace PrintIt.Core.Tests.Pdfium
             }
 
             // Assert
-            bitmap.Save("dummy-output.png", ImageFormat.Png);
+            // bitmap.Save("dummy-snapshot.png", ImageFormat.Png);
+            CompareWithSnapshot(bitmap, "dummy-snapshot.png").Should().BeTrue();
+        }
+
+        private static bool CompareWithSnapshot(Bitmap bitmap, string snapshotImageName)
+        {
+            using var snapshot = (Bitmap) Image.FromStream(GetEmbeddedResourceStream(snapshotImageName));
+
+            if (snapshot.Width != bitmap.Width || snapshot.Height != bitmap.Height)
+                return false;
+
+            for (var y = 0; y < snapshot.Height; y++)
+                for (var x = 0; x < snapshot.Width; x++)
+                    if (snapshot.GetPixel(x, y) != bitmap.GetPixel(x, y))
+                        return false;
+            
+            return true;
         }
         
         private static Stream GetEmbeddedResourceStream(string name)
