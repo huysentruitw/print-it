29eac9c7ae8de893331f3b56acb14efbbd3ae95e
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2019-12-27 13:43:04 +0100

    Add CommandService

---
+++ src/PrintIt.Core/CommandService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+
+namespace PrintIt.Core
+{
+    internal sealed class CommandService : ICommandService
+    {
+        private readonly ILogger<CommandService> _logger;
+
+        public CommandService(ILogger<CommandService> logger)
+        {
+            _logger = logger;
+        }
+
+        public CommandExecutionResult Execute(string fileName, params string[] arguments)
+        {
+            _logger.LogInformation("Executing {FileName} with arguments {Arguments}", fileName, arguments);
+
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = fileName,
+                Arguments = string.Join(" ", arguments),
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+            };
+
+            using var process = Process.Start(startInfo);
+
+            if (process == null)
+            {
+                return new CommandExecutionResult(Array.Empty<string>(), -1);
+            }
+
+            process.WaitForExit();
+
+            return new CommandExecutionResult(
+                output: ReadLinesWithContent(process.StandardOutput)
+                    .Concat(ReadLinesWithContent(process.StandardError))
+                    .ToArray(),
+                exitCode: process.ExitCode);
+        }
+
+        private static IEnumerable<string> ReadLinesWithContent(StreamReader streamReader)
+        {
+            while (!streamReader.EndOfStream)
+            {
+                string line = streamReader.ReadLine()?.Trim() ?? string.Empty;
+                if (!string.Empty.Equals(line))
+                    yield return line;
+            }
+        }
+    }
+
+    public interface ICommandService
+    {
+        CommandExecutionResult Execute(string fileName, params string[] arguments);
+    }
+
+    public sealed class CommandExecutionResult
+    {
+        internal CommandExecutionResult(string[] output, int exitCode)
+        {
+            Output = output;
+            ExitCode = exitCode;
+        }
+
+        public string[] Output { get; }
+
+        public int ExitCode { get; }
+
+        public bool Success => ExitCode == 0;
+    }
+}
---
+++ tests/PrintIt.Core.Tests/CommandServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace PrintIt.Core.Tests
+{
+    public sealed class CommandServiceTests
+    {
+        [Fact]
+        public void Execute_BasicCommand_ShouldReturnOutput()
+        {
+            // Arrange
+            var commandService = new CommandService(new Mock<ILogger<CommandService>>().Object);
+            var fileName = $@"{Environment.GetFolderPath(Environment.SpecialFolder.System)}\ipconfig.exe";
+
+            // Act
+            CommandExecutionResult executionResult = commandService.Execute(fileName);
+
+            // Assert
+            executionResult.Success.Should().BeTrue();
+            executionResult.Output.Should().Contain("Windows IP Configuration");
+        }
+
+        [Fact]
+        public void Execute_CommandWithArguments_ShouldReturnCorrectOutput()
+        {
+            // Arrange
+            var commandService = new CommandService(new Mock<ILogger<CommandService>>().Object);
+            var fileName = $@"{Environment.GetFolderPath(Environment.SpecialFolder.System)}\whoami.exe";
+            var arguments = new[] { "/user" };
+
+            // Act
+            CommandExecutionResult executionResult = commandService.Execute(fileName, arguments);
+
+            // Assert
+            executionResult.Success.Should().BeTrue();
+            executionResult.Output.Should().Contain("USER INFORMATION");
+        }
+
+        [Fact]
+        public void Execute_ErrorInCommand_ShouldNotBeSuccessful()
+        {
+            // Arrange
+            var commandService = new CommandService(new Mock<ILogger<CommandService>>().Object);
+            var fileName = $@"{Environment.GetFolderPath(Environment.SpecialFolder.System)}\whoami.exe";
+            var arguments = new[] { "blabla" };
+
+            // Act
+            CommandExecutionResult executionResult = commandService.Execute(fileName, arguments);
+
+            // Assert
+            executionResult.Success.Should().BeFalse();
+            var output = string.Join(Environment.NewLine, executionResult.Output);
+            output.Should().Contain("blabla");
+        }
+    }
+}
