175b3e4db51ac4fb57d3292959fd7b6e3e671f97
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2020-05-04 19:17:22 +0200

    Add PrintController

---
+++ src/PrintIt.ServiceHost/Controllers/PrintController.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PrintIt.Core;
+
+namespace PrintIt.ServiceHost.Controllers
+{
+    [ApiController]
+    [Route("print")]
+    public class PrintController : ControllerBase
+    {
+        private readonly IPdfPrintService _pdfPrintService;
+
+        public PrintController(IPdfPrintService pdfPrintService)
+        {
+            _pdfPrintService = pdfPrintService;
+        }
+
+        [HttpPost]
+        [Route("from-pdf")]
+        public async Task<IActionResult> PrintFromPdf([FromForm] PrintFromTemplateRequest request)
+        {
+            await using Stream pdfStream = request.PdfFile.OpenReadStream();
+            _pdfPrintService.Print(pdfStream, request.PrinterPath);
+            return Ok();
+        }
+    }
+
+    public sealed class PrintFromTemplateRequest
+    {
+        [Required]
+        public IFormFile PdfFile { get; set; }
+
+        [Required]
+        public string PrinterPath { get; set; }
+    }
+}
